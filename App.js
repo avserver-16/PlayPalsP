406039168fa15070c56bd6394dd9b77890f229b5
avserver-16 <avish.vijay2021@gmail.com>
2025-02-21 10:47:15 +0530

    Merged main and Jinay_Screens App.js

--- App.js
+++ App.js
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack"; 
+import { useFonts, Kanit_400Regular } from "@expo-google-fonts/kanit";
+import * as SplashScreen from "expo-splash-screen";
+import "react-native-gesture-handler";
+
+// Prevent splash screen from auto-hiding before fonts are loaded
+SplashScreen.preventAutoHideAsync();
+
+import Welcome from "./screens/Welcome";
+import Login from "./screens/Login";
+import LangSport from "./screens/LangSport";
 import SignUp from "./screens/Signup";
 import OtpVerification from "./screens/OtpVerification";
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Kanit_400Regular,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignUp">
-        <Stack.Screen
-          name="SignUp"
-          component={SignUp}
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen
-          name="OtpVerification"
-          component={OtpVerification}
-          options={{ headerShown: false }} 
-        />
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name="LangSport" component={LangSport} options={{ headerShown: false }} />
+        <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
+        <Stack.Screen name="OtpVerification" component={OtpVerification} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
